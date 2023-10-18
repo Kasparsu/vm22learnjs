ece995123ad83f7156027f05573390f9e14a6b4e
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-10-18 13:52:57 +0300

    euler 2

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+let num1 = 0;
+let num2 = 1;
 let sum = 0;
-for(let i=0; i<1000; i++){
-    if(i%3===0 || i%5===0 ){
-        sum+=i;
+while(num2<4_000_000){
+    if(num2%2===0){
+        sum+=num2;
     }
+    let temp = num2;
+    num2 = num1+num2;
+    num1 = temp;
 }
 console.log(sum);
